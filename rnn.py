wrong dims without predict

rnn.py
@@ -2,23 +2,25 @@
 import numpy as np
 import gensim
 import time
+import math
 import theano
 import theano.tensor as T
 from collections import defaultdict
 
 word_vec_len = 200 #dim of word vec
-hidden_len = 4096 #dim of hidden layer
+hidden_len = 400 #dim of hidden layer
 output_len =  64402 #num of words, not sure yet
 lr = 0.1
 epoch = 10
 sigma = lambda x: 1/ (1+T.exp(-x))
 rng = np.random.RandomState(1234)
-v = T.matrix(dtype=theano.config.floatX)
-target = T.matrix(dtype=theano.config.floatX)
-
+dtype = theano.config.floatX
+v = T.fvector()
+target = T.matrix(dtype=dtype)
+params = []
 
 def soft_max(x):
-    deno = [math.exp(temp) for temp in x].sum()
+    deno = T.sum(T.exp(x))
     return x/deno
 
 def init_w(size_x, size_y):
@@ -26,18 +28,17 @@ def init_w(size_x, size_y):
     return values
 
 def get_param(word_vec_len, hidden_len, output_len):
-    W_xh = theano.shared(init_w(word_vec_len, hidden_len))
-    W_hy = theano.shared(init_w(hidden_len, output_len))
+    W_xh = theano.shared(init_w(hidden_len, word_vec_len))
+    W_hy = theano.shared(init_w(output_len, hidden_len))
     W_hh = theano.shared(init_w(hidden_len, hidden_len))
     b_y = theano.shared(init_w(hidden_len, 1))
     b_h = theano.shared(init_w(output_len, 1))
-    h0 = theano.shared(np.zeros(hidden_len, dtype = theano.config.floatX))
     return [W_xh, W_hy, W_hh, b_y, b_h]
 
 def one_rnn_step( x, h_tm, W_xh, W_hy, W_hh, b_y, b_h):
     #compute the output layer and the hidden layer of the RNN
-    hh = T.dot(x, W_xh) + T.dot(h_tm, W_hh) + b_h
-    yy = sigma(T.dot(hh, W_hy) + b_y)
+    hh = theano.dot(W_xh, x) + theano.dot(W_hh, h_tm) + b_h
+    yy = sigma(theano.dot(hh, W_hy) + b_y)
     return [soft_max(yy), hh]
 
 def get_word2vec_model(path='../word2vec-read-only/vectors.bin'):
@@ -45,8 +46,9 @@ def get_word2vec_model(path='../word2vec-read-only/vectors.bin'):
     word2vec = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
     return word2vec
 
-def get_train_func(cost, v, target, params):
+def get_train_func(cost, v, target):
     #get the learning function ==> learn_rnn_fn
+    #output the cost, and update the params
     gparams = []
     for param in params:
         gparam = T.grad(cost, param)
@@ -61,29 +63,31 @@ def get_train_func(cost, v, target, params):
             )
     return learn_rnn_fn
 
-def predict(line): # the function for jacky82226
+#def predict(line): # the function for jacky82226
     #return the probability of the sentence
     #(multiply all the prob of the words in the sentence)
     #param:
     #       type: list
     #       line: list of the words of the sentence  
 def main():
-   
-    [params, h0] = get_param(word_vec_len, hidden_len, output_len, learning_rate)
+    
+    [W_xh, W_hy, W_hh, b_y, b_h] = get_param(word_vec_len, hidden_len, output_len)
+    params = [W_xh, W_hy, W_hh, b_y, b_h]
+    h0 = theano.shared(np.zeros((hidden_len,1), dtype = dtype))
     # init the params 
     word2vec = get_word2vec_model()
     # the word2vec model
     index_word_mapping = dict(zip(word2vec.index2word, range(0, output_len)))
     # mapping of words and index EX: 1 for 'the', 2 for 'is'
-    [y_vals, h_vals], _ = thenao.scan(fn = one_rnn_step,
-        sequences = v,
+    [y_vals, h_vals], _ = theano.scan(fn = one_rnn_step,
+        truncate_gradient = 4,
+        sequences = dict(input=v, taps=[0]),
         outputs_info = [h0, None], #no output layer 
-        non_sequences = [W_xh, W_hy, W_hh, b_y, b_h],
-        truncate_gradient = 4)
+        non_sequences = params)
     cost = -T.mean(target * T.log(y_vals) + (1. - target) * T.log(1. - y_vals))
-    learn_rnn_fn = get_train_func(cost, v, target, params)
+    learn_rnn_fn = get_train_func(cost, v, target)
     #above is for BPTT
-    out = np.zeros(output_len,1)
+    out = np.zeros((output_len,1), dtype=dtype)
     # for labeling the ground truth
     with open('train.txt', 'r') as f:
         for i in range(epoch):
@@ -91,14 +95,15 @@ def main():
             for line in f:
                 words = line.strip().split()
                 for i in range(0, len(line)-1):
-                    w_t = word2vec[line[i]]
-                    out[index_word_mapping[word2vec[line[i+1]]]] = 1
+                    w_t = word2vec[words[i]]
+                    print words[i+1]
+                    out[index_word_mapping[words[i+1]]] = 1
                     learn_rnn_fn(w_t, out)
-                    out[index_word_mapping[word2vec[line[i+1]]]] = 0
+                    out[index_word_mapping[words[i+1]]] = 0
 
     return 0
 if __name__ == '__main__':
-    return(main())
+    exit(main())
 #    for line in f:
 #        for word in line.split():
 #            if word != '<s>' and word != '</s>':